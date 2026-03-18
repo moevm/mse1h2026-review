ad82964cea2bc901474260c95379c9ea82ab3fa1
DaniilLutfulin <alebardavormit@gmail.com>
2026-03-18 05:26:44 +0300

    integrated consumer with rabbit (#33)

--- consumer/ai_review_runner.py
+++ consumer/ai_review_runner.py
@@ -8,29 +8,39 @@ from datetime import datetime
 
 def run_ai_review_for_pr(repo_url: str, repo_name:str, repo_owner:str, pr_number: str, branch: str):
 
-    temp_dir = tempfile.mkdtemp(prefix="ai-review-")
+    temp_dir = tempfile.mkdtemp(prefix=f"ai-review-{repo_name}")
     try:
+        github_token = os.getenv("GITHUB_TOKEN")
+        if not github_token:
+            raise ValueError("GITHUB_TOKEN environment variable is not set")
+        if repo_url.startswith("https://"):
+            auth_repo_url = repo_url.replace("https://", f"https://{github_token}@")
+        else:
+            auth_repo_url = repo_url
+            
         print(f"[{datetime.now()}] Cloning repo {repo_url} (branch {branch}) into {temp_dir}", flush=True)
         subprocess.run(
-            ["git", "clone", "--branch", branch, repo_url, temp_dir],
-            check=True
+            ["git", "clone", "--branch", branch, auth_repo_url, temp_dir],
+            check=True,
+            capture_output=True,
+            text=True
         )
 
         config = {
             "llm": {
                 "provider": "OLLAMA",
                 "meta": {
-                    "model": "minimax-m2.5:cloud",
-                    "max_tokens": 2000,
+                    "model": "qwen2.5-coder:1.5b",
+                    "max_tokens": 5000,
                     "temperature": 0.3,
-                    "num_ctx": 2048,
+                    "num_ctx": 5000,
                     "top_p": 0.9,
                     "repeat_penalty": 1.1,
                     "stop": ["USER:", "SYSTEM:"],
                     "seed": 42
                 },
                 "http_client": {
-                    "api_url": "http://localhost:11434"
+                    "api_url": "http://ollama:11434"
                 }
             },
             "vcs": {
@@ -52,12 +62,32 @@ def run_ai_review_for_pr(repo_url: str, repo_name:str, repo_owner:str, pr_number
                 }
             },
             "core": {"concurrency": 7},
+            "prompt": {
+                "context": {},
+                "normalize_prompts": True,
+                "context_placeholder": "<<{value}>>",
+                "include_inline_system_prompts": True,
+                "include_context_system_prompts": True,
+                "include_summary_system_prompts": True,
+                "include_inline_reply_system_prompts": True,
+                "include_summary_reply_system_prompts": True,
+                "inline_prompt_files": ["/app/prompt.md"]
+            },
+
             "review": {
                 "mode": "FULL_FILE_DIFF",
                 "dry_run": False,
                 "inline_tag": "#ai-review-inline",
-                "summary_tag": "#ai-review-summary"
+                "inline_reply_tag": "#ai-review-inline-reply",
+                "summary_tag": "#ai-review-summary",
+                "summary_reply_tag": "#ai-review-summary-reply",
+                "context_lines": 10,
+                "allow_changes": [],
+                "ignore_changes": [],
+                "review_removed_marker": " # removed",
+                "inline_comment_fallback": True
             },
+
             "artifacts": {
                 "llm_dir": "./artifacts/llm",
                 "llm_enabled": True
@@ -69,6 +99,8 @@ def run_ai_review_for_pr(repo_url: str, repo_name:str, repo_owner:str, pr_number
             json.dump(config, f, indent=2)
 
         print(f"[{datetime.now()}] Running ai-review for PR #{pr_number}", flush=True)
+        subprocess.run(["ai-review", "clear-inline"], cwd=temp_dir, check=True)
+        subprocess.run(["ai-review", "show-config"], cwd=temp_dir, check=True)
         subprocess.run(["ai-review", "run-inline"], cwd=temp_dir, check=True)
         print(f"[{datetime.now()}] Finished AI review for PR #{pr_number}", flush=True)
         #TODO: запрос в БД для сохранения информации о выполнении ревью (время, результат, артефакты модели)
--- consumer/worker.py
+++ consumer/worker.py
@@ -1,33 +1,34 @@
 import os
 import pika
+import json
+from ai_review_runner import run_ai_review_for_pr
 
 def callback(ch, method, properties, body):
-    print("Hello world from consumer!")
-    print("Received message:", body.decode(), flush=True)
-    ch.basic_ack(delivery_tag=method.delivery_tag)
+    try:
+        message = json.loads(body)
+        print(f"Received message: {message}", flush=True)
+        run_ai_review_for_pr(
+            repo_url=message["repo_url"],
+            repo_name=message["repo_name"],
+            repo_owner=message["repo_owner"],
+            pr_number=message["pr_number"],
+            branch=message["branch"]
+        )
+        
+        ch.basic_ack(delivery_tag=method.delivery_tag)
+    except Exception as e:
+        print(f"Error processing message: {e}", flush=True)
 
 def main():
-    rabbitmq_host = os.getenv("RABBITMQ_HOST", "rabbitmq")
-    queue_name = os.getenv("QUEUE_NAME", "ai-review")
-    rabbit_user = os.getenv("RABBIT_USER", "guest")
-    rabbit_pass = os.getenv("RABBIT_PASS", "guest")
+    queue_name = os.getenv("QUEUE_NAME", "webhook_queue")
+    rabbitmq_url = os.getenv("RABBIT_URL", "amqp://guest:guest@rabbitmq:5672/")
     
-    credentials = pika.PlainCredentials(rabbit_user, rabbit_pass)
-    connection = pika.BlockingConnection(
-        pika.ConnectionParameters(
-            host=rabbitmq_host,
-            credentials=credentials
-        )
-    )
-    
+    params = pika.URLParameters(rabbitmq_url)
+    connection = pika.BlockingConnection(params)
     channel = connection.channel()
     channel.queue_declare(queue=queue_name, durable=True)
-    
-    channel.basic_consume(
-        queue=queue_name,
-        on_message_callback=callback
-    )
-    
+
+    channel.basic_consume(queue=queue_name, on_message_callback=callback)
     print(f"Consumer started. Waiting for messages on {queue_name}...", flush=True)
     channel.start_consuming()
 
