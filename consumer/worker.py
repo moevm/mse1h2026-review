1f4039becba16e3d64bc7886e9ae9811e3b3dc3f
DaniilLutfulin <alebardavormit@gmail.com>
2026-03-20 11:17:49 +0300

    added branch finding (#33)

--- consumer/worker.py
+++ consumer/worker.py
@@ -1,7 +1,17 @@
 import os
 import pika
 import json
+import requests
 from ai_review_runner import run_ai_review_for_pr
+
+def get_branch(repo, pr, token):
+    url = f"https://api.github.com/repos/{repo}/pulls/{pr}"
+    headers = {"Authorization": f"token {token}"} if token else {}
+    
+    r = requests.get(url, headers=headers)
+    if r.status_code == 200:
+        return r.json()["head"]["ref"]
+    return None
 
 def callback(ch, method, properties, body):
     try:
@@ -17,6 +27,12 @@ def callback(ch, method, properties, body):
         pr_number = message.get("issue", {}).get("number")
         repo_full_name = message.get("repository", {}).get("full_name")
         repo_url = message.get("repository", {}).get("html_url")
+        token = os.getenv("GITHUB_TOKEN")
+        branch = get_branch(repo_full_name, pr_number, token)
+        if not branch:
+            print(f"Branch not found for PR #{pr_number}", flush=True)
+            ch.basic_ack(delivery_tag=method.delivery_tag)
+            return
         
         missing_fields = []
         if not pr_number:
@@ -32,13 +48,14 @@ def callback(ch, method, properties, body):
         
         repo_owner, repo_name = repo_full_name.split("/")
         
-        print(f"Processing PR #{pr_number} in {repo_full_name}", flush=True)
+        print(f"Processing PR #{pr_number} in {repo_full_name}, branch: {branch}", flush=True)
         
         run_ai_review_for_pr(
             repo_url=repo_url,
             repo_name=repo_name,
             repo_owner=repo_owner,
             pr_number=pr_number,
+            branch=branch
         )
         
         ch.basic_ack(delivery_tag=method.delivery_tag)
