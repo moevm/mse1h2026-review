b068e746a8c0949355b130f5f3787a48675f429d
Sofia Titkova <sdtitkova@stud.etu.ru>
2026-05-19 21:35:12 +0300

    fix linter

--- frontend/app/vite.config.js
+++ frontend/app/vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
@@ -12,12 +12,12 @@ export default defineConfig({
         target: 'http://127.0.0.1:8000',
         changeOrigin: true,
         secure: false,
-        configure: (proxy, _options) => {
-          proxy.on('error', (err, _req, _res) => {
-            console.log('[PROXY ERROR]', err);
+        configure: (proxy) => {
+          proxy.on('error', (err) => {
+            console.log('proxy error', err);
           });
         },
       }
     }
   }
-})
+});
